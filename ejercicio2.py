fd7ad695c0dde8cfe6004e586474c97684fb1a48
jhogerolmos <jhogerolmos@gmail.com>
2025-07-03 17:10:42 -0500

    fix error price, add class Product

--- ejercicio2.py
+++ ejercicio2.py
@@ -17,25 +17,41 @@ class SummerSale(Promotion):
     def apply(self, total: float) -> float:
         return total - (total*0.20)
 
+class Product():
+    def __init__(self, name: str, price: float):
+        if price <= 0:
+            raise ValueError("El precio debe ser mayor a cero")
+        self.__name = name
+        self.__price = price
+
+    def get_name(self):
+        return self.__name
+
+    def get_price(self):
+        return self.__price
+
 
 class ShoppingCart:
     def __init__(self):
         self.__items = []
 
-    def add(self, product, price):
-        self.__items.append((product,price))
+    def add(self, product: Product):
+        self.__items.append(product)
 
     def calcular_total(self, promotion:Promotion = None):
-        total = sum(precio for _, precio in self.__items)
+        total = sum(product.get_price() for product in self.__items)
         if promotion:
             return promotion.apply(total)
         return total
 
+p1 = Product("Juguete", 28000)
+p2 = Product("Camisa", 65000)
+p3 = Product("Xiaomi redmi note 14 pro plus", 1300000)
 
 shopping_cart = ShoppingCart()
-shopping_cart.add("Juguete", 28000)
-shopping_cart.add("Camisa", 65000)
-shopping_cart.add("Xiaomi redmi note 14 pro plus", 1300000)
+shopping_cart.add(p1)
+shopping_cart.add(p2)
+shopping_cart.add(p3)
 
 print("Total del carrito sin descuento:")
 print(shopping_cart.calcular_total())
